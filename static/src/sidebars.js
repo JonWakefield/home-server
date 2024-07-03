0c4cc0dfe7356435fda17b365826528480d44cf5
Jon Wakefield <jonwakefield.mi@gmail.com>
2024-07-03 11:43:08 -0600

    added create acc modal

--- static/src/sidebars.js
+++ static/src/sidebars.js
@@ -7,7 +7,31 @@
   })
 })()
 
+document.addEventListener('DOMContentLoaded', (event) => {
 
-// (() => {
-  // 
-// })
+  // Get the modal
+  var modalOpener = document.getElementById("createAccModal");
+
+  // Get the button that opens the modal
+  var openModalBtn = document.getElementById("openAccModal");
+
+  // Get the <span> element that closes the modal
+  var closeModalBtn = document.getElementsByClassName("btn-close")[0];
+
+  // When the user clicks on the button, open the modal
+  openModalBtn.onclick = function() {
+    modalOpener.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  closeModalBtn.onclick = function() {
+    modalOpener.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modalOpener) {
+        modalOpener.style.display = "none";
+      }
+  }
+});
